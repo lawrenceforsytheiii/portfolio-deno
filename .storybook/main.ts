c32259f0144291fb1ec9f288c63f6bf68380a300
Lawrence Forsythe <lawrenceforsytheiii@gmail.com>
2024-11-05 08:50:24 -0700

    feat: create and add initial storybook main config

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import { Storybook } from "https://esm.sh/@storybook/react";
+import tailwindConfig from "../tailwind.config.ts";
+
+export default {
+  framework: "@storybook/react",
+  stories: ["../stories/**/*.stories.tsx"],
+  addons: ["@storybook/addon-essentials"],
+  webpackFinal: (config: { module: { rules: { test: RegExp; use: string[]; }[]; }; }) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: ["babel-loader"],
+    });
+    return config;
+  },
+  postcss: { plugins: [tailwindConfig] },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import { type Config } from "tailwindcss";
+import typography from "https://esm.sh/@tailwindcss/typography";
+import forms from "https://esm.sh/@tailwindcss/forms";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
+    "components/{atoms,molecules,organisms,layouts,pages}/**/*.{ts,tsx}",
+    "stories/{atoms,molecules,organisms,layouts,pages}/**/*.{ts,tsx}",
   ],
+  plugins: [typography, forms],
 } satisfies Config;
