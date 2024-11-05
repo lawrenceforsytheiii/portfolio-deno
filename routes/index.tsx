1384481520ba8036128ab2baad7f89c07fb42152
Lawrence Forsythe <lawrenceforsytheiii@gmail.com>
2024-11-05 09:17:43 -0700

    chore: update index route to include Button component

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,15 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+/** @jsx h */
+import { h } from "preact";
+import MuiThemeProvider from "../components/MuiThemeProvider.tsx";
+import Button from "../components/Button.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <MuiThemeProvider>
+      <div>
+        <h1>Welcome to My Fresh App</h1>
+        <Button label="Go to About" color="primary" variant="contained" />
       </div>
-    </div>
+    </MuiThemeProvider>
   );
 }
